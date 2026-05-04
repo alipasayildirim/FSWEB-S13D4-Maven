e3aa5afa0b8a4fd1890f1864aa4934f4b1f16915
Ali Paşa Yıldırım <alipasayildirimm@gmail.com>
2026-05-04 17:58:11 +0300

    Sprint 13 Day 4 project completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point first = new Point(6, 5);
+
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+
+        System.out.println("distance(second)= " + first.distance(second));
+
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0,0);
+
+        System.out.println("distance()= " + point.distance());
+
+        System.out.println("-----------------------------------------------------");
+
+        Player p1 = new Player("Aragorn", 100, Weapon.SWORD);
+
+        System.out.println(p1.healthRemaining()); // 100
+
+        p1.loseHealth(80);
+        System.out.println(p1.healthRemaining()); // 20
+
+        p1.loseHealth(50);
+
+        System.out.println(p1.healthRemaining()); // 0
+
+        Player p2 = new Player("Gimli", 50, Weapon.AXE);
+        p2.restoreHealth(70);
+        System.out.println(p2.healthRemaining());
     }
 }
---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public enum Weapon {
+    SWORD(85,1.5),
+    AXE(100,0.5);
+
+    private final int damage;
+    private final double attackSpeed;
+
+    Weapon(int damage,double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+}
